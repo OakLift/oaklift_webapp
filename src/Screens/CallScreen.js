2cadb903977a6bd0039fb75f0334981d1763e72c
aanandak97 <aanandak97@andrew.cmu.edu>
2024-03-03 00:27:25 -0800

    Update CallScreen.js

--- src/Screens/CallScreen.js
+++ src/Screens/CallScreen.js
@@ -8,37 +8,52 @@ import {
   usePublish,
   useRemoteUsers,
   AgoraRTCProvider,
-  useRTCClient
+  useRTCClient,
+  useClientEvent
 } from "agora-rtc-react";
 import { useState } from "react";
 
 function CallScreen() {
-  
   const remoteUsers = useRemoteUsers();
+  const agoraEngine = useRTCClient();
   const { isLoading: isLoadingCam, localCameraTrack } = useLocalCameraTrack();
   const queryParameters = new URLSearchParams(window.location.search);
   const { isLoading: isLoadingMic, localMicrophoneTrack } =
     useLocalMicrophoneTrack();
-  const [joined, setJoined] = useState(false);
 
-  usePublish([localMicrophoneTrack, localCameraTrack]);
-  console.log(queryParameters.get("userID"))
-  const deviceLoading = isLoadingMic || isLoadingCam;
   useJoin({
     appid: "cf36a471064d48c38b5b4a05e89d7fc0",
     channel: "testingroom",
     token:
-      "007eJxTYHBuYkxcuYn323Epz+15NV3R38+cZWKSrlC4sNOy1qpBlUuBITnN2CzRxNzQwMwkxcQi2dgiyTTJJNHANNXCMsU8LdlAifFJakMgI8OjSDZGRgYIBPG5GUpSi0sy89KL8vNzGRgAemYfxQ==",
+      "007eJxTYHg4bbeXbexdy7PsLmHrA92npDnOvrVM8OWk1X7Cm1iS+wMUGJLTjM0STcwNDcxMUkwsko0tkkyTTBINTFMtLFPM05INklWepDYEMjJM4bjDyMgAgSA+N0NJanFJZl56UX5+LgMDAPMnIYM=",
+  }, true);
+
+  useClientEvent(agoraEngine, "user-joined", (user) => {
+    console.log("The user", user.uid, " has joined the channel");
   });
+
+  useClientEvent(agoraEngine, "user-left", (user) => {
+    console.log("The user", user.uid, " has left the channel");
+  });
+
+  useClientEvent(agoraEngine, "user-published", (user, mediaType) => {
+    console.log("The user", user.uid, " has published media in the channel");
+  });
+
+  usePublish([localMicrophoneTrack, localCameraTrack]);
+  console.log(queryParameters.get("userID"));
+  const deviceLoading = isLoadingMic || isLoadingCam;
   return (
     <div>
       {deviceLoading ? (
         <div>Loading devices...</div>
       ) : (
         <div id="videos">
+          {/* Render the local video track */}
           <div className="vid" style={{ height: 300, width: 600 }}>
             <LocalVideoTrack track={localCameraTrack} play={true} />
           </div>
+          {/* Render remote users' video and audio tracks */}
           {remoteUsers.map((remoteUser) => (
             <div
               className="vid"
@@ -60,9 +75,9 @@ function CallScreenWithProvider() {
   );
   return (
     <AgoraRTCProvider client={client}>
-      <CallScreen/>
+      <CallScreen />
     </AgoraRTCProvider>
-  )
+  );
 }
 
-export default CallScreenWithProvider
+export default CallScreenWithProvider;
