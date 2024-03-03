ca91c3f046fc32bbe6ec7615b5c4ecd0d3b3036f
aanandak97 <aanandak97@andrew.cmu.edu>
2024-03-03 00:53:35 -0800

    Changes

--- src/Screens/RouteList.js
+++ src/Screens/RouteList.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
 import CallScreenWithProvider from "./CallScreen";
+import AgoraUI from "./AgoraCalls";
 
 function RouteList() {
   return (
     <Routes>
       <Route path="/" element={<HomeScreen />} />
-      <Route path="/call/:room" element={<CallScreenWithProvider />} /> 
+      <Route path="/call/:room" element={<AgoraUI />} /> 
     </Routes>
   );
 }
