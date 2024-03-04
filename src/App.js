7b764212c19d0c497a1b2da395fa21b73f76aa7e
Kevin Tang <kevintang300@gmail.com>
2024-03-03 18:43:27 -0800

    auth0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import LoginButton from "./components/LoginButton"
 import LogoutButton from "./components/LogoutButton"
 import Profile from "./components/profile";
+import './App.css'; 
 
 function App() {
   return (
-    <main classname="column">
-      <h1>Auth0 login</h1>
-      <LoginButton/>
-      <LogoutButton/>
-      <Profile/>
-    </main>
+    <div className="app-container">
+      <header className="header">
+        <h1>Auth0 Login</h1>
+      </header>
+      <main className="content">
+        <LoginButton/>
+        <LogoutButton/>
+        <Profile/>
+      </main>
+    </div>
   );
 }
 
