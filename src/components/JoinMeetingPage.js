d6f985dfee50625225244cb70d9a822504ce74f5
Kevin Tang <kevintang300@gmail.com>
2024-03-09 21:04:33 -0800

    kdev: join meeting improvs

--- src/components/JoinMeetingPage.js
+++ src/components/JoinMeetingPage.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import './JoinMeetingPage.css'; // Import the CSS file
 
 const JoinMeetingPage = () => {
-    return (
-        <div>
-            <label>
-                Meeting Link:
-                <input type="text"></input>
-            </label>
-
-            <label>
-                Token:
-                <input type="text"></input>
-            </label>
-
-            <label>
-                Channel ID:
-                <input type="text"></input>
-            </label>
-        </div>
-    );
+  return (
+    <div className="join-meeting-page">
+      <div>
+        <label htmlFor="meetingLink">
+          Meeting Link:
+          <input type="text" id="meetingLink" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="token">
+          Token:
+          <input type="text" id="token" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="channelId">
+          Channel ID:
+          <input type="text" id="channelId" />
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default JoinMeetingPage;
